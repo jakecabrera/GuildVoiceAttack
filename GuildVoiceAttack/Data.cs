6a69f37d5b8682b3499b6ecf131e0414f748a503
jake cabrera <jakestengercabrera@gmail.com>
2018-11-25 14:40:52 -0800

    Add project files.

---
+++ GuildVoiceAttack/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace GuildVoiceAttack
+{
+    internal class Data
+    {
+        public string name { get; set; }
+        public string age { get; set; }
+        public string sex { get; set; }
+    }
+}
